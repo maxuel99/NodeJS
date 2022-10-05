e436c4e5b3c2d13ea7f2e9130549035cdcb17ddf
maxuel99 <massimocaser@gmail.com>
2022-10-05 18:37:18 +0200

    add NodeJSREPL

---
+++ RandomID.js
@@ -0,0 +1,5 @@
+const crypto = require('crypto');
+
+const uuid = crypto.randomUUID();
+
+console.log(uuid);
--- Sum.js
+++
@@ -1,6 +0,0 @@
-function Sum(a, b) {
-    let sum = a + b
-    console.log(`La somma di 5 + 6 è ${sum}`)
-}
-
-Sum(5, 6);
