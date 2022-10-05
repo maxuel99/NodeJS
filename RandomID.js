ccd92b01ce8b64f503bedcd1df17a1ed8a30d238
maxuel99 <massimocaser@gmail.com>
2022-10-05 18:47:13 +0200

    add CommonJSModules

--- RandomID.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto');
-
-const uuid = crypto.randomUUID();
-
-console.log(uuid);
---
+++ script.js
@@ -0,0 +1,3 @@
+const outputMessage = require('./outputMessage.js');
+
+outputMessage("Hello, World!");
