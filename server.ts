8bbc4c21240d78e3d48f6f08b2ec0a1b76063f07
maxuel99 <massimocaser@gmail.com>
2022-10-10 14:18:12 +0200

    add LogData

--- server.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-
-import app from './app';
-
-const port = process.env.PORT;
-
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localost:${port}`)
-});
